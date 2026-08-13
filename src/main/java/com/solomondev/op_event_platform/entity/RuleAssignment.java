1049ae9362887eb65f266457420e07f3c9067bdf
keenbiscuit <solomon.hudson25@gmail.com>
2026-08-13 12:30:17 -0500

    Removed unused imports

--- src/main/java/com/solomondev/op_event_platform/entity/RuleAssignment.java
+++ src/main/java/com/solomondev/op_event_platform/entity/RuleAssignment.java
@@ -1,9 +1,6 @@
 package com.solomondev.op_event_platform.entity;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
