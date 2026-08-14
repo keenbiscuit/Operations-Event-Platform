7bffe91cde92dcebeb5b73e764fcfb7f870c3fbd
keenbiscuit <solomon.hudson25@gmail.com>
2026-08-14 16:48:59 -0500

    removing unused imports

--- src/main/java/com/solomondev/op_event_platform/entity/AlertEvent.java
+++ src/main/java/com/solomondev/op_event_platform/entity/AlertEvent.java
@@ -2,7 +2,6 @@ package com.solomondev.op_event_platform.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -33,6 +32,5 @@ public class AlertEvent {
     private Event event;
 
     private LocalDateTime linkedAt;
-
 
 }
