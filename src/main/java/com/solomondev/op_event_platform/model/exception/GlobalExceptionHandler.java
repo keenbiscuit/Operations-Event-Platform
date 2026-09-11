0ee1fcc30eb3b0ca6496b8b8c65b4a3410d1684b
keenbiscuit <solomon.hudson25@gmail.com>
2026-09-11 15:00:23 -0500

    Remove unused imports

--- src/main/java/com/solomondev/op_event_platform/model/exception/GlobalExceptionHandler.java
+++ src/main/java/com/solomondev/op_event_platform/model/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.solomondev.op_event_platform.model.exception;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
