de794b8ee308d926ad362f50fd97e4ee3f9a66a4
keenbiscuit <solomon.hudson25@gmail.com>
2026-09-16 14:26:39 -0500

    fix: add rule operator migration

--- src/main/resources/db/migration/V2__add_rule_operator.sql
+++ src/main/resources/db/migration/V2__add_rule_operator.sql
@@ -1,4 +1,2 @@
--- V2__add_rule_operator.sql
--- Add the operator column to the rules table
-
-ALTER TABLE rules ADD COLUMN operator VARCHAR(30) NOT NULL;
+ALTER TABLE rules
+ADD COLUMN operator VARCHAR(50) NOT NULL DEFAULT 'GREATER_THAN';
